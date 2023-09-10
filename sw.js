47844fed56d845c54a900c3af296602499665455
Xa2r <arifinannas6@gmail.com>
2023-09-10 15:39:05 +0000

    sw

---
+++ sw.js
@@ -0,0 +1,33 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/assets/*',
+  '/images/*',
+  'https://*.js'
+];
+
+// install event
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+        //console.log(keys);
+        return Promise.all(keys
+            .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+            .map(key => caches.delete(key))
+        );
+        })
+    );
+});
